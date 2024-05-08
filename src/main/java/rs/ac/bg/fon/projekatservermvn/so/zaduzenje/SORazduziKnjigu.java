e4f49713f14b57c2ec4bf837649d2dee2e96da2f
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 16:54:55 +0200

    Dokumentovana sistemska operacija RazduziKnjigu.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SORazduziKnjigu.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SORazduziKnjigu.java
@@ -11,10 +11,15 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.Zaduzenje;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vrsi razduzivanje odredjenog zaduzenja u bazi podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SORazduziKnjigu extends OpstiSO {
     
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Zaduzenje,
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
        if(!(odo instanceof Zaduzenje)){
@@ -22,6 +27,10 @@ public class SORazduziKnjigu extends OpstiSO {
        } 
     }
     
+    /**
+     * Vrsi razduzivanje odredjenog zaduzenja (prosledjenog). Razduzivanje se vrsi tako sto se postavlja datum razduzenja na trenutni datum.
+     * Prilikom razduzivanja se vrsi izmena primerka knjige u bazi podataka kako bi se oznacilo da je primerak knjige ponovo dostupan.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         DBBroker.getInstanca().update(odo);
