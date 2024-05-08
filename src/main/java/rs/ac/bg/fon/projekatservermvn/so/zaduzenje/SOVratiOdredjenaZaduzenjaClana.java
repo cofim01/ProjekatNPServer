d3d69adc4df7f1a258f5960ca36c74fe00a6fbe0
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 17:21:19 +0200

    Dokumentovana sistemska operacija VratiOdredjenaZaduzenjaClana.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SOVratiOdredjenaZaduzenjaClana.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SOVratiOdredjenaZaduzenjaClana.java
@@ -11,18 +11,28 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.Zaduzenje;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vraca odredjena zaduzenja za jednog clana iz baze podataka sortirana u opadajucem redosledu prema datumu zaduzenja iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOVratiOdredjenaZaduzenjaClana extends OpstiSO {
+    /**
+     * Lista svih zaduzenja clana koji ispunjavaju prosledjeni uslov kao ArrayList.
+     */
     private ArrayList<Zaduzenje> lista = new ArrayList<>();
 
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Zaduzenje.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Zaduzenje)) {
             throw new Exception("Prosledjeni objekat nije instanca klase zaduzenje.");
         }
     }
-
+    /**
+     * Vraca sva zaduzenja iz baze podataka koja odgovaraju prosledjenom uslovu i dodaje ih u listu.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> zaduzenja = DBBroker.getInstanca().selectPoUslovu(odo);
@@ -31,7 +41,10 @@ public class SOVratiOdredjenaZaduzenjaClana extends OpstiSO {
         }
 
     }
-
+    /**
+     * Vraca listu odredjenih zaduzenja clana.
+     * @return - lista odredjenih zaduzenja clana kao ArrayList.
+     */
     public ArrayList<Zaduzenje> getLista() {
         return lista;
     }
