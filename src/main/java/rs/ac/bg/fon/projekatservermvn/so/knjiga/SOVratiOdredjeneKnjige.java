cbcc142e2c74df574defe6f83f6f4c1a8a8f5a31
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 15:20:53 +0200

    Dokumentovana sistemska operacija VratiOdredjeneKnjige.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOVratiOdredjeneKnjige.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOVratiOdredjeneKnjige.java
@@ -11,19 +11,29 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vraca odredjene knjige iz baze podataka (po uslovu). 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOVratiOdredjeneKnjige extends OpstiSO {
-
+    /**
+     * Lista svih knjiga iz baze podataka koje zadovoljavaju uslov kao ArrayList.
+     */
     ArrayList<Knjiga> lista=new ArrayList<>();
 
+     /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Knjiga.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Knjiga)) {
             throw new Exception("Prosledjeni objekat nije instanca knjige!");
         }
     }
-
+    /**
+     * Vraca sve knjige iz baze koje ispunjavaju uslov i dodaje ih u listu.
+     * Uslov je ispunjen ukoliko ime knjige sadrzi ime prosledjenog objekta.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> knjige = DBBroker.getInstanca().selectPoUslovu(odo);
@@ -31,7 +41,10 @@ public class SOVratiOdredjeneKnjige extends OpstiSO {
             lista.add((Knjiga) k);
         }
     }
-
+    /**
+     * Vraca listu svih knjiga iz baze koje zadovoljavaju uslov.
+     * @return - lista svih knjiga po uslovu kao ArrayList.
+     */
     public ArrayList<Knjiga> getLista() {
         return lista;
     }
