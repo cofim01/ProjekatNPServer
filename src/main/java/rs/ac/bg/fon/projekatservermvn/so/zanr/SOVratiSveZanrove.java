a68492da255bcbbd92b1e20064b81a79347d20a0
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 17:34:44 +0200

    Dokumentovana sistemska operacija VratiSveZanrove.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/zanr/SOVratiSveZanrove.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/zanr/SOVratiSveZanrove.java
@@ -10,19 +10,28 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.Zanr;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vraca sve zanrove iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOVratiSveZanrove extends OpstiSO {
-
+    /**
+     * Lista svih zanrova iz baze podataka kao ArrayList.
+     */
     ArrayList<Zanr> lista = new ArrayList<>();
 
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Zanr.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Zanr)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Zanr");
         }
     }
-
+    /**
+     * Vraca sve zanrove iz baze podataka na osnovu prosledjenog objekta i dodaje ih u listu zanrova.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> zanrovi = DBBroker.getInstanca().select(odo);
@@ -30,7 +39,10 @@ public class SOVratiSveZanrove extends OpstiSO {
             lista.add((Zanr) z);
         }
     }
-
+    /**
+     * Vraca listu svih zanrova.
+     * @return lista svih zanrova kao ArrayList.
+     */
     public ArrayList<Zanr> getLista() {
         return lista;
     }
