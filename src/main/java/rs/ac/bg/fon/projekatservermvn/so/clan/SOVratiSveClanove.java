7ad546541718e0a4e3295e411a84b8a76f3456b6
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 14:51:13 +0200

    Dokumentovana sistemska operacija VratiSveeClanove.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/clan/SOVratiSveClanove.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/clan/SOVratiSveClanove.java
@@ -10,19 +10,28 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vraca sve clanove biblioteke iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOVratiSveClanove extends OpstiSO {
-
+    /**
+     * Lista svih clanova iz baze podataka kao ArrayList.
+     */
     ArrayList<Clan> lista = new ArrayList<>();
 
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Clan.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Clan)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Clan");
         }
     }
-
+    /**
+     * Vraca sve clanove biblioteke iz baze podataka i dodaje ih u listu.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> clanovi = DBBroker.getInstanca().select(odo);
@@ -30,7 +39,10 @@ public class SOVratiSveClanove extends OpstiSO {
             lista.add((Clan) c);
         }
     }
-
+    /**
+     * Vraca listu svih clanova biblioteke.
+     * @return - lista svih clanova kao ArrayList.
+     */
     public ArrayList<Clan> getLista() {
         return lista;
     }
