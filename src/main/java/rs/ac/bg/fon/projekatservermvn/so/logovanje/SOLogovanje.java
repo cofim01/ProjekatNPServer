ed87e87e122246f0b148844090e1fcd9102bd1c0
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 16:32:08 +0200

    Dokumentovana sistemska operacija Logovanje.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/logovanje/SOLogovanje.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/logovanje/SOLogovanje.java
@@ -11,19 +11,31 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja loguje korisnika na sistem. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOLogovanje extends OpstiSO {
-
+    /**
+     * Podaci o korisniku koji se ulogovao na sistem.
+     */
     Korisnik korisnik;
-
+    
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Korisnik.
+     * @param odo
+     * @throws Exception 
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Korisnik)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Korisnik");
         }
     }
-
+    /**
+     * Loguje korisnika na sistem ako postoji prosledjeni korisnik u bazi podataka za datim podacima.
+     * Ukoliko postoji korisnik u bazi sa prosledjenim podacima, postavlja sve podatke ulogovanog korisnika.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> lista = DBBroker.getInstanca().select(odo);
@@ -44,7 +56,10 @@ public class SOLogovanje extends OpstiSO {
         }
         throw new Exception("Neuspesno logovanje, nepostojeći korisnik.");
     }
-
+    /**
+     * Vraca sve podatke o ulogovanom korisniku.
+     * @return - ulogovani korisnik.
+     */
     public Korisnik getKorisnik() {
         return korisnik;
     }
