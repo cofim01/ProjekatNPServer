407eb4d0cc5eeb7a665f829871ac8230f7a35a8e
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 12:20:20 +0200

    Dokumentovana opsta sistemska operacija.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/OpstiSO.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/OpstiSO.java
@@ -7,14 +7,33 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Apstraktna klasa koja predstavlja osnovu za sve sistemske operacije.
+ * Svaka sistemska operacija se sastoji od validacije i izvrsenja odredjenih akcija nad objektima domenskog modela.
  */
 public abstract class OpstiSO {
-
+    
+    /**
+     * Apstraktna metoda za proveru uslova koji su nephodni kako bi se sistemka operacija izvrsila.
+     * @param odo - prosledjeni objekat za koji se vrsi validacija.
+     * @throws Exception - ukoliko validacija nije uspesna.
+     */
     protected abstract void validate(OpstiDomenskiObjekat odo) throws Exception;
 
+    /**
+     * Apstraktna metoda koja predstavlja izvrsenje sistemske operacije nad prosledjenim objektom domenskog modela.
+     * @param odo - prosledjeni objekat domenskog modela.
+     * @throws Exception - ukoliko je doslo do greske prilikom izvrsenja sistemske operacije. 
+     */
     protected abstract void execute(OpstiDomenskiObjekat odo) throws Exception;
 
+    /**
+     * Metoda koja predstavlja sablon za sve sistemske operacije.
+     * Satoji se od validacije prosledjenog objekata,izvrsenja sistemske operacije nad objektom kao i potvrde promena nad bazom podataka.
+     * Ukoliko dodje do greske prilikom izvrsenja operacije, promene se ponistavaju.
+     * @param odo - prosledjeni objekat domenskog modela.
+     * @throws Exception - ukoliko dodje do greske prilikom izvrsenja sistemske operacije.
+     */
     public void executeTemplate(OpstiDomenskiObjekat odo) throws Exception {
         try {
             validate(odo);
@@ -26,12 +45,19 @@ public abstract class OpstiSO {
         }
 
     }
-
+    /**
+     * Potvrdjuje izmene u bazi podataka.
+     * @throws SQLException - ukoliko dodje do greske prilikom potvrde izmena u bazi.
+     */
     private void commit() throws SQLException {
         DBBroker.getInstanca().getConection().commit();
 
     }
-
+    
+    /**
+     * Ponistava izmene u bazi podataka.
+     * @throws SQLException - ukoliko dodje do greske prilikom ponistavanja izmena u bazi.
+     */
     private void rollback() throws SQLException {
         DBBroker.getInstanca().getConection().rollback();
     }
