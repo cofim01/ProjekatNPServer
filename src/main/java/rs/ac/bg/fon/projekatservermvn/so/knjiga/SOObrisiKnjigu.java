f55d870b21cb831e44556c9f1bca4fd1a5599e2a
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 15:14:02 +0200

    Dokumentovana sistemska operacija ObrisiKnjigu.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOObrisiKnjigu.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOObrisiKnjigu.java
@@ -10,17 +10,24 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja brise odredjenu knjigu iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOObrisiKnjigu extends OpstiSO{
-
+    
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Knjiga.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if(!(odo instanceof  Knjiga)){
             throw new Exception("Prosledjeni objekat nije instaca knjige!");
         }
     }
-
+    /**
+     * Brise odredjenu knjigu iz baze podataka na osnovu primernog kljuca prosledjenog objekta.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         DBBroker.getInstanca().delete(odo);
