2d88f4eb2a65841497d5698ff47513189db66f51
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 17:31:06 +0200

    Dokumentovana sistemska operacija ZaduziKnjigu.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SOZaduziKnjigu.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/zaduzenje/SOZaduziKnjigu.java
@@ -10,17 +10,25 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.Zaduzenje;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja zaduzuje odredjenu knjigu u bazi podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOZaduziKnjigu extends OpstiSO {
-
+    
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Zaduzenje.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof Zaduzenje)) {
             throw new Exception("Prosledjeni objekat nije instanca klase Zaduzenje");
         }
     }
-
+    /**
+     * Vrsi zaduzivanje odredjenog primerka knjige na osnovu prosledjenog objekta. Zaduzivanje se vrsi tako sto se postavlja datum zaduzenja na trenutni datum.
+     * Prilikom zaduzivanja se vrsi izmena primerka knjige u bazi podataka kako bi se oznacilo da je primerak knjige trenutno nedostupan.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         DBBroker.getInstanca().insert(odo);
