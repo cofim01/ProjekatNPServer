3410d7a6c88eda89ee8047bf5b34caf4a3ab0bd4
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 14:46:44 +0200

    Dokumentovana sistemska operacija VratiOdredjeneClanove.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/clan/SOVratiOdredjeneClanove.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/clan/SOVratiOdredjeneClanove.java
@@ -10,14 +10,31 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 
 
-
+/**
+ * 
+ * @author MRDAK-PC
+ * Predstavlja sistemsku operaciju koja vraca odredjene clanove (po uslovu) iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
+ */
 public class SOVratiOdredjeneClanove extends OpstiSO {
+    /**
+     * Lista trazenih clanova biblioteke prema prosledjenom uslovu kao ArrayList.
+     */
     private ArrayList<Clan> lista=new ArrayList<>();
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Clan.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
-        
+        if (!(odo instanceof Clan)) {
+            throw new Exception("Prosledjeni objekat nije instanca klase Clan!");
+        }
     }
-
+    /**
+     * Vraca sve clanove biblioteke koji odgovaraju prosledjenom uslovu i dodaje ih u listu.
+     * Vracaju se svi clanovi koji u svom imenu ili prezimenu sadrze ime i prezime koji su prosledjeni kroz objekat domenskog modela.
+     * 
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> clanovi = DBBroker.getInstanca().selectPoUslovu(odo);
@@ -25,7 +42,10 @@ public class SOVratiOdredjeneClanove extends OpstiSO {
             lista.add((Clan) c);
         }
     }
-
+    /**
+     * Vraca listu clanova koji odgovaraju uslovu.
+     * @return - lista clanova kao ArrayList
+     */
     public ArrayList<Clan> getLista() {
         return lista;
     }
