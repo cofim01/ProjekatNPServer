6d073d20157d9725aea5e29acc8f02e626894a2b
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 15:10:53 +0200

    Dokumentovana sistemska operacija IzmeniKnjigu.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOIzmeniKnjigu.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/knjiga/SOIzmeniKnjigu.java
@@ -10,17 +10,24 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vrsi izmenu nad odredjenom knjigom u bazi podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOIzmeniKnjigu extends OpstiSO{
-
+    
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase Knjiga.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if(!(odo instanceof  Knjiga)){
             throw new Exception("Prosledjeni objekat nije instanca klase knjiga");
         }
     }
-
+    /**
+     * Azurira odredjene podatke vec postojece knjige u bazi podataka na osnovu prosledjenog objekta.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         DBBroker.getInstanca().update(odo);
