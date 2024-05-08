dc5af7f54766ce67fefde8dfc78a7af1785e61ec
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 16:47:57 +0200

    Dokumentovana sistemska operacija VratiSvePrimerkeKnjige.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/primerci/SOVratiSvePrimerkeKnjige.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/primerci/SOVratiSvePrimerkeKnjige.java
@@ -11,19 +11,29 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.PrimerakKnjige;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja vraca sve primerke odredjene knjige iz baze podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOVratiSvePrimerkeKnjige extends OpstiSO {
-    
+    /**
+     * Lista svih primeraka odredjene knjige u bazi podataka kao ArrayList.
+     */
     ArrayList<PrimerakKnjige> lista=new ArrayList<>();
     
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase PrimerakKnjige.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof PrimerakKnjige)) {
             throw new Exception("Proesledjeni objekat nije instaca klase PrimerakKnjige!");
         }
     }
-
+    
+    /**
+     * Vraca sve primerke odredjene knjige iz baze podataka na osnovu prosledjenog objekta i dodaje ih u listu primeraka.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         ArrayList<OpstiDomenskiObjekat> primerciKnjige=DBBroker.getInstanca().selectPoUslovu(odo);
@@ -33,7 +43,10 @@ public class SOVratiSvePrimerkeKnjige extends OpstiSO {
         
         
     }
-
+    /**
+     * Vraca listu svih primerka odredjene knjige iz baze podataka.
+     * @return - lista svih primeraka odredjene knjige kao ArrayList.
+     */
     public ArrayList<PrimerakKnjige> getLista() {
         return lista;
     }
