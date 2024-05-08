38eabc1ab34508389c9c8a73ec7f379322c09446
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-08 16:41:28 +0200

    Dokumentovana sistemska operacija IzmeniPrimerak.

--- src/main/java/rs/ac/bg/fon/projekatservermvn/so/primerci/SOIzmeniPrimerak.java
+++ src/main/java/rs/ac/bg/fon/projekatservermvn/so/primerci/SOIzmeniPrimerak.java
@@ -10,17 +10,23 @@ import rs.ac.bg.fon.projekatzajednickimvn.domen.PrimerakKnjige;
 
 /**
  *
- * @author MRDAK-PC
+ * @author Filip Mrdak
+ * Predstavlja sistemsku operaciju koja menja podatke za odredjeni primerak knjige u bazi podataka. 
+ * Nasledjuje apstraktnu klasu OpstiSo i implementira metode validate i execute.
  */
 public class SOIzmeniPrimerak extends OpstiSO{
-
+    /**
+     * Validacija je uspesna ukoliko je prosledjeni objekat klase PrimerakKnjige.
+     */
     @Override
     protected void validate(OpstiDomenskiObjekat odo) throws Exception {
         if (!(odo instanceof PrimerakKnjige)) {
             throw new Exception("Proesledjeni objekat nije instaca klase PrimerakKnjige!");
         }
     }
-
+    /**
+     * Vrsi izmene nad postojecim primerkom knjige u bazi podataka na osnovu prosledjenog objekta.
+     */
     @Override
     protected void execute(OpstiDomenskiObjekat odo) throws Exception {
         DBBroker.getInstanca().update(odo);
