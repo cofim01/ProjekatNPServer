b86a3ffe937bef08cbccca0ddecdc89e5268b9e3
filip-mrdak <fm20200181@student.fon.bg.ac.rs>
2024-05-11 17:08:06 +0200

    Testirana sistemska operacija VratiSveIzdavace.

---
+++ src/test/java/rs/ac/bg/fon/projekatservermvn/so/izdavac/SOVratiSveIzdavaceTest.java
@@ -0,0 +1,81 @@
+
+package rs.ac.bg.fon.projekatservermvn.so.izdavac;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import rs.ac.bg.fon.projekatservermvn.db.DBBroker;
+import rs.ac.bg.fon.projekatzajednickimvn.domen.Clan;
+import rs.ac.bg.fon.projekatzajednickimvn.domen.Izdavac;
+import rs.ac.bg.fon.projekatzajednickimvn.domen.Knjiga;
+import rs.ac.bg.fon.projekatzajednickimvn.domen.OpstiDomenskiObjekat;
+
+
+public class SOVratiSveIzdavaceTest {
+    
+    SOVratiSveIzdavace so;
+    public SOVratiSveIzdavaceTest() {
+        so=new SOVratiSveIzdavace();
+    }
+    
+    
+    @Mock
+    private DBBroker dbBrokerMock;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        DBBroker.setInstanca(dbBrokerMock);
+    }
+    
+    @Test
+    public void testValidateNeodgovarajuciObjekat() {
+        OpstiDomenskiObjekat odo = new Knjiga();
+        
+        Exception e = assertThrows(java.lang.Exception.class, () -> so.validate(odo));
+        assertEquals("Prosledjeni objekat nije instanca klase Izdavac!", e.getMessage());
+    }
+
+    @Test
+    public void testValidateOk() {
+        OpstiDomenskiObjekat odo=new Izdavac();
+        try {
+            so.validate(odo);
+            assertTrue(true);
+        } catch (Exception ex) {
+            fail("Neocekivana greska prilikom validacije.");
+        }
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        ArrayList<OpstiDomenskiObjekat> izdavaci = new ArrayList<>();
+        Izdavac i=new Izdavac();
+        i.setIzdavacId(5);
+        i.setNaziv("Laguna");
+        izdavaci.add(i);
+        try {
+            when(dbBrokerMock.select(any(OpstiDomenskiObjekat.class))).thenReturn(izdavaci);
+            so.execute(new Izdavac());
+        } catch (Exception ex) {
+            fail("Neocekivana greska prilikom izvrsavanja so.");
+        }
+        
+        verify(dbBrokerMock).select(any(OpstiDomenskiObjekat.class)); 
+        verifyNoMoreInteractions(dbBrokerMock);
+        
+        assertEquals(1, so.getLista().size());
+        assertEquals(5, so.getLista().get(0).getIzdavacId());
+        assertEquals("Laguna", so.getLista().get(0).getNaziv());
+
+    }
+
+    
+}
